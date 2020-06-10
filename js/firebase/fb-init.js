5d9d3ed6ce27d256b812c1509541f9f306fd3b10
neolgu <32592676+neolgu@users.noreply.github.com>
2020-06-10 22:26:47 +0900

    Add login & test
    Add login
    test ID: asdf@asdf.com PW: asdfasdf

---
+++ js/firebase/fb-init.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAspvGfKAxlbbXvMie7R1UoaMd9mq18VIA",
+    authDomain: "se-back-end.firebaseapp.com",
+    databaseURL: "https://se-back-end.firebaseio.com",
+    projectId: "se-back-end",
+    storageBucket: "se-back-end.appspot.com",
+    messagingSenderId: "741818179578",
+    appId: "1:741818179578:web:238494855f0d0e3cafce43",
+    measurementId: "G-LBLPDRP27Z"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
