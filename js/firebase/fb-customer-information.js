6de95d4b664d8fd95fc0d70cfb0bee8475924dab
heejin ryu <jiin20803@gmail.com>
2020-06-16 01:06:00 +0900

    customer information search function and delete function from fb

--- js/firebase/fb-customer-information.js
+++ js/firebase/fb-customer-information.js
@@ -25,10 +25,60 @@ function addCustomer(evt){
     .catch(function(error){
         alert("Error adding document: ",error);
     });
-
+    $('#name').val("");
+    $("#birth").val("");
+    $("#contact-number").val("");
 }
+
+function searchCustomer(){
+    var name = $("#search-name").val();
+    var birth = $("#search-birth").val();
+    
+    db.collection("Customer").where("customerName","==",name).where("customerBirth","==",birth).get()
+    .then(function (querySnapshot){
+       querySnapshot.forEach(function (doc){
+           data = doc.data();
+           $("#txt1").val(data["customerName"]);
+           $("#txt2").val(data["customerBirth"]);
+           $("#txt3").val(data["customerPhoneNo"]);
+           $("#txt4").val(data["sell"]);
+           $("#txt5").val(data["reservation"]);
+           $("#txt6").val(data["consulting"]);
+           $("#txt7").val(data["points"]);
+           $("#txt8").val(data["coupons"]);
+       })
+       $("#search-name").val("");
+       $("#search-birth").val("");
+    })
+    .catch(function(error){
+        alert(error);
+    });
+}
+
+function deleteCustomer(){
+    var name = $("#txt1").val();
+    var birth = $("#txt2").val();
+    if(name||birth !== null){
+
+        db.collection("Customer").where("customerName","==",name).where("customerBirth","==",birth).get()
+        .then(function(querySnapshot){
+            querySnapshot.forEach(function (doc) {
+                doc.ref.delete();
+                alert("successfully deleted");
+                console.log("successfully deleted");
+            });
+        })
+        .catch(function(error){
+            alert(error);
+        });
+    }
+    
+}
+
 window.onload = function () {
     document.getElementById('add-customer-btn').addEventListener('click', addCustomer,false);
+    document.getElementById('search-btn').addEventListener('click', searchCustomer,false);
+    document.getElementById('delete-customer-btn').addEventListener('click', deleteCustomer,false);
 }
 
 var db = firebase.firestore();
