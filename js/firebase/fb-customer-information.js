a666ff5085134c29aec0eba2f94e6505619596fb
heejin ryu <jiin20803@gmail.com>
2020-06-16 01:10:48 +0900

    move change button name function to js file

--- js/firebase/fb-customer-information.js
+++ js/firebase/fb-customer-information.js
@@ -75,10 +75,55 @@ function deleteCustomer(){
     
 }
 
+function editCustomer() {
+    if( document.getElementById("edit-customer-btn").innerText  == "완료"){
+        document.getElementById("edit-customer-btn").innerText  = "EDIT";
+        var oEle1 = document.getElementById('txt1') ;
+        oEle1.readOnly = true ;
+        var oEle2 = document.getElementById('txt2') ;
+        oEle2.readOnly = true ;
+        var oEle3 = document.getElementById('txt3') ;
+        oEle3.readOnly = true ;
+        var oEle4 = document.getElementById('txt4') ;
+        oEle4.readOnly = true ;
+        var oEle5 = document.getElementById('txt5') ;
+        oEle5.readOnly = true ;
+        var oEle6 = document.getElementById('txt6') ;
+        oEle6.readOnly = true ;
+        var oEle7 = document.getElementById('txt7') ;
+        oEle7.readOnly = true ;
+        var oEle8 = document.getElementById('txt8') ;
+        oEle8.readOnly = true ;
+  
+    }
+    else if( document.getElementById("edit-customer-btn").innerText  == "EDIT"){
+        document.getElementById("edit-customer-btn").innerText  = "완료";
+        document.getElementById("edit-customer-btn").background= "blue";
+        var oEle1 = document.getElementById('txt1') ;
+        oEle1.readOnly = false ;
+        var oEle2 = document.getElementById('txt2') ;
+        oEle2.readOnly = false ;
+        var oEle3 = document.getElementById('txt3') ;
+        oEle3.readOnly = false ;
+        var oEle4 = document.getElementById('txt4') ;
+        oEle4.readOnly = false ;
+        var oEle5 = document.getElementById('txt5') ;
+        oEle5.readOnly = false ;
+        var oEle6 = document.getElementById('txt6') ;
+        oEle6.readOnly = false ;
+        var oEle7 = document.getElementById('txt7') ;
+        oEle7.readOnly = false ;
+        var oEle8 = document.getElementById('txt8') ;
+        oEle8.readOnly = false ;
+
+    }
+}
+
 window.onload = function () {
     document.getElementById('add-customer-btn').addEventListener('click', addCustomer,false);
     document.getElementById('search-btn').addEventListener('click', searchCustomer,false);
     document.getElementById('delete-customer-btn').addEventListener('click', deleteCustomer,false);
+    document.getElementById('edit-customer-btn').addEventListener('click', editCustomer,false);
 }
 
 var db = firebase.firestore();
