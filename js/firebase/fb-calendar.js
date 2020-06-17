ebd49cfdeecd2ca4c353b726f23e8746e0ddf533
neolgu <32592676+neolgu@users.noreply.github.com>
2020-06-17 20:37:28 +0900

    Add reservation to db
    Add reservation to db

--- js/firebase/fb-calendar.js
+++ js/firebase/fb-calendar.js
@@ -15,6 +15,28 @@ function getSchedules() {
     });
 }
 
+function addSchedule() {
+
+    var name = document.getElementById("Name").value;
+    var date = document.getElementById("Date").value;
+    var time = document.getElementById("Time").value;
+    var dr = document.getElementById("Doctor").value;
+
+    if (name != "" && date != "" && time != "" && dr != "") {
+        db.collection("Reservation").add({
+            "date": date,
+            "docName": dr,
+            "patientName": name,
+            "time": time,
+            "state": "",
+        }).then(function () {
+            window.location.reload();
+        });
+    }
+
+
+}
+
 var db = firebase.firestore();
 
 var schedules = new Array();
