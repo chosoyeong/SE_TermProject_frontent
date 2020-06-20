6f8012c31b522d19acb3395daabc70fdfbb692fa
neolgu <32592676+neolgu@users.noreply.github.com>
2020-06-20 15:53:15 +0900

    Edit Reservation Calendar
    Event(schedule) information is loaded window.
    And add Edit, delete function.

--- js/firebase/fb-calendar.js
+++ js/firebase/fb-calendar.js
@@ -4,13 +4,19 @@ function getSchedules() {
     db.collection("Reservation").orderBy("time").get().then(function (querySnapshot) {
         querySnapshot.forEach(function (doc) {
 
-            console.log(doc.id, "=>", doc.data());
+            //console.log(doc.id, "=>", doc.data());
 
             calendar.addEvent({
                 'title': doc.data()["patientName"] + "\n Dr." + doc.data()["docName"] + "\n Time:" + doc.data()["time"],
                 'start': doc.data()["date"],
-                'end': doc.data()["date"]
+                'end': doc.data()["date"],
+                'id': doc.id,
+                'date':doc.data()["date"]
             });
+
+            events = calendar.getEvents();
+
+            console.log(events);
         });
     });
 }
@@ -34,7 +40,34 @@ function addSchedule() {
         });
     }
 
+}
 
+function editSchedule(id) {
+
+    var name = document.getElementById("Name_edit").value;
+    var date = document.getElementById("Date_edit").value;
+    var time = document.getElementById("Time_edit").value;
+    var dr = document.getElementById("Doctor_edit").value;
+
+    if (name != "" && date != "" && time != "" && dr != "") {
+        db.collection("Reservation").doc(id).set({
+            "date": date,
+            "docName": dr,
+            "patientName": name,
+            "time": time,
+            "state": "",
+        }).then(function () {
+            console.log("OK");
+            window.location.reload();
+        });
+    }
+}
+
+function deleteSchedule(id) {
+
+    db.collection("Reservation").doc(id).delete().then(function () {
+        window.location.reload();
+    });
 }
 
 var db = firebase.firestore();
